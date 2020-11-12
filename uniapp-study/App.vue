320df4e6404959790be4b7cf1b41d8d70460027f
wqy <1950016253@qq.com>
2020-11-12 16:37:16 +0800

    音乐播放页面

--- uniapp-study/App.vue
+++ uniapp-study/App.vue
@@ -15,7 +15,12 @@
 <style>
 	/*每个页面公共css */
 	@import url("./common/css/free.css");
+	/*矢量图标库css */
 	@import url("./common/css/iconfont.css");
+	/*css动画库 */
 	@import url("./common/css/animate.css");
+	/*自定义css动画库 */
 	@import url("./common/css/myanimate.css");
+	/*自定义css主题 */
+	@import url("./common/css/theme.css");
 </style>
